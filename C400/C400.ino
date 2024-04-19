73c8ec2bfd961c991e3273c14f20e447598ef808
RobWPink <robert.pink@oneh2.com>
2024-04-19 13:31:50 -0400

    update to be like C200 code

---
+++ C400/C400.ino
@@ -0,0 +1,262 @@
+#include "C400.h"
+void setup() {
+  Serial.begin(9600);
+  pinModeSetup();
+  Wire.begin();
+  matrixSetup("C400", "V0.1.1");
+  i2cSetup();
+
+  Serial.println("OK");
+  DO_Comm_LSR_Local = true;
+  delay(3000);
+  printMode = PACKET;
+ 
+}
+
+void loop() {
+  if(!loopTimer){loopTimer = millis();}
+
+  SerialCLI();
+  if(lsrReset){
+    if(lsrReset == 1){lsrReset = millis();DO_Comm_LSR_Reset = true;}
+    if(millis() - lsrReset > 1000){
+      DO_Comm_LSR_Reset = false;
+      lsrReset = 0;
+    }
+  }
+  i2cTransceive(250);
+    
+  if(!DI_Encl_ESTOP){STATE = ESTOP;}
+  flashDriver();
+  
+  dataPrint(delayTime);
+  daughterPrint(delayTime);
+  //variable max psi for discharge3 using suction2
+  if(AI_H2_psig_PT712_Stage1_DischargeTank > 1600){PTdata[1].max = 2.8571*AI_H2_psig_PT712_Stage1_DischargeTank + 6128.6;} //different from burbank
+  else{PTdata[1].max = 7.1429*AI_H2_psig_PT712_Stage1_DischargeTank - 657.14;}
+  if(PTdata[1].max > 0){PTdata[1].maxRecovery = PTdata[1].max - 250;}
+
+  if(STATE != MANUAL_CONTROL){
+    DO_Encl_PilotAmber = DI_Comm_LSR_Local;
+    DO_CLT_PMP104_PMP204_CoolantPumps_Enable = DI_Comm_LSR_Local;
+    DO_CLT_FCU112_CoolantFan1_Enable = DI_Comm_LSR_Local;
+    DO_CLT_FCU212_CoolantFan2_Enable = DI_Comm_LSR_Local;
+    if(DI_Comm_LSR_Local && (STATE == PRODUCTION || STATE == PAUSE)){
+      PREV_STATE = STATE;
+      STATE = FAULT;
+      faultString = "LSR Error";
+    }
+  }
+  //########################STATE MACHINE################################
+  if(STATE != CHANGED_STATE){ //reset state timer
+    timer[0] = 0;
+    timer[1] = 0;
+    timer[3] = 0;
+    timer[4] = 0;
+    timer[5] = 0;
+
+    DO_Encl_PilotRed = false;
+    DO_Encl_PilotGreen = false;
+    DO_Encl_PilotAmber = false;
+
+    flashGreen = 0;
+    flashRed = 0;
+    flashAmber = 0;
+
+    CHANGED_STATE = STATE;
+  }
+
+  String errDev[30] = {""};
+  switch(STATE){
+    case IDLE_OFF:
+      if(!timer[0]){ timer[0] = millis(); }
+
+      smallMatrix[2].displayPlay(DI_Comm_LSR_Local);
+      DO_Encl_PilotGreen = !DI_Comm_LSR_Local;
+      DO_Encl_PilotAmber = DI_Comm_LSR_Local;
+    
+      if(DI_Encl_ButtonGreen || virtualGreenButton){ STATE = IDLE_ON; }
+    break;
+
+  //#####################################################################
+
+    case IDLE_ON:
+      if(!timer[0]){
+        timer[0] = millis();
+        DO_HYD_PMP458_HydraulicPump1_Enable = true; //turn on hydraulic pump 1
+        DO_HYD_PMP552_HydraulicPump2_Enable = true; //turn on hydraulic pump 2 
+        DO_H2_XV907_SuctionPreTank = true;
+        flashGreen = 500;
+      }
+      if(millis() - timer[0] > 5000 && timer[0]){ //after 5 seconds
+        if(!DI_HYD_IS000_HydraulicFilterSwitch){ //check filter switch
+          STATE = FAULT;
+          faultString = "Filter Switch Error";
+        }
+        else{STATE = PRODUCTION;}
+      }
+    break;
+
+  //#####################################################################
+
+    case PRODUCTION:
+      if(!timer[0]){
+        timer[0] = millis();
+        flashGreen = 1000;
+        //DO_HYD_XV460_DCV1_A = false;
+        //DO_HYD_XV463_DCV1_B = false;
+        DO_HYD_XV554_DCV2_A = false;
+        DO_HYD_XV557_DCV2_B = false;
+        //INTENSE1 = START;
+        INTENSE2 = START;
+        timer[5] = millis();
+      }
+      if(!timer[1]){timer[1] = millis();}
+      if(millis() - timer[1] > 60000 && timer[1]){
+        highCycleCnt_ = highCycleCnt/2;
+        //lowCycleCnt_ = lowCycleCnt/2;
+        highCycleCnt = 0;
+        //lowCycleCnt = 0;
+        timer[1] = 0;
+      }
+
+      //if the red button is held for less than 5 seconds pause it otherwise safe shutdown
+      if(DI_Encl_ButtonRed && !prevR){
+        prevR = true;
+        if(!holdR){holdR = millis();}
+      }
+      if(!DI_Encl_ButtonRed && prevR){
+        prevR = false;
+        if(millis() - holdR < 5000 && holdR){
+          INTENSE2 = PAUSE;
+          manualPause = true;
+        }
+        else{STATE = SHUTDOWN;}
+        holdR = 0;
+      }
+
+      switch(INTENSE2){
+        case OFF:
+          DO_HYD_XV554_DCV2_A = false;
+          DO_HYD_XV557_DCV2_B = false;
+        break;
+        
+        case START:
+          DO_HYD_XV554_DCV2_A = true;
+          DO_HYD_XV557_DCV2_B = false;
+          timer[4] = millis();
+          smallMatrix[2].displayHollowPlay(false);
+          INTENSE2 = ON;
+        break;
+
+        case ON:
+          if(millis() - timer[4] > 500 && timer[4] && AI_HYD_psig_PT561_HydraulicInlet2 >= switchingPsi2){ //switching pressure
+            DO_HYD_XV554_DCV2_A = !DO_HYD_XV554_DCV2_A;
+            DO_HYD_XV557_DCV2_B = !DO_HYD_XV557_DCV2_B;
+            timer[4] = millis();
+            highCycleCnt++;
+          }
+          if(millis() - timer[4] > 60000){ STATE = IDLE_OFF; }
+        break;
+
+        case PAUSE:
+          DO_HYD_XV554_DCV2_A = false;
+          DO_HYD_XV557_DCV2_B = false;
+          smallMatrix[2].displayPause(false);
+          for(int i = 0; i < PTsize;i++){
+            if(!manualPause && PTdata[i].overPressure && (PTdata[i].pause == 2 || !PTdata[i].pause)){
+              break;
+            }
+          }
+          INTENSE2 = START;
+        break;
+
+        default:
+        break;
+      }
+    break;
+
+  //#####################################################################
+
+    case SHUTDOWN:
+      if(!timer[0]){
+        timer[0] = millis();
+        for(int i = 0; i < DOsize;i++){
+          if((DOdata[i].key.indexOf("XV") >= -1 || DOdata[i].key.indexOf("PMP") >= -1 || DOdata[i].key.indexOf("PL") >= -1) && DOdata[i].key.indexOf("LSR") == -1 && DOdata[i].key.indexOf("CLT") == -1){
+            *DOdata[i].value = false;
+          }
+        }
+
+        smallMatrix[2].displayStop(false);
+      }
+      
+      if(millis() - timer[0] > 30000 && timer[0]){
+        for(int i = 0; i < DOsize;i++){
+          if((DOdata[i].key.indexOf("XV") >= -1 || DOdata[i].key.indexOf("PMP") >= -1 || DOdata[i].key.indexOf("PL") >= -1) && DOdata[i].key.indexOf("CLT") == -1){
+            *DOdata[i].value = false;
+          }
+        }
+        timer[0] = 0;
+        timer[1] = 0;
+        timer[3] = 0;
+        timer[4] = 0;
+        DO_CLT_PMP104_PMP204_CoolantPumps_Enable = false;
+        STATE = IDLE_OFF;
+      }
+      //if pressure lowers and temps lower shut down the rest
+    break;
+
+  //#####################################################################
+
+    case MANUAL_CONTROL:
+      if(!timer[0]){
+        timer[0] = millis();
+        smallMatrix[2].displayChar('M', false);
+        Serial.println("manual");
+      }
+    break;
+
+  //#####################################################################
+
+    case FAULT:
+      if(!timer[0]){
+        timer[0] = millis();
+        for(int i = 0; i < DOsize;i++){
+          *DOdata[i].value = false;
+        }
+        flashRed = 500;
+        smallMatrix[2].displayStop(true);
+      }
+      
+      if(DI_Encl_ButtonRed){STATE = IDLE_OFF;}
+    break;
+
+  //#####################################################################
+
+    case ESTOP:
+      if(!timer[0]){
+        timer[0] = millis();
+        for(int i = 0; i < DOsize;i++){
+          *DOdata[i].value = false;
+        }
+        DO_Encl_PilotRed = true;
+        smallMatrix[2].displayStop(false);
+      }
+      for(int i = 0; i < DOsize;i++){
+        if(DOdata[i].key.indexOf("PL") == -1){
+          *DOdata[i].value = false;
+        }
+      }
+      if(DI_Encl_ESTOP){STATE = IDLE_OFF;}
+    break;
+
+  //#####################################################################
+
+    default:
+      if(errorPrint){Serial.println("STATE ERROR!!!!!");}
+      break;
+  }
+
+  loopTime = millis() - loopTimer;
+  loopTimer = 0;
+}
---
+++ C400/SerialCLI.ino
@@ -0,0 +1,348 @@
+#define SPTR_SIZE   20
+void SerialCLI() {
+  String str;
+  char *argBuf[20] = { 0 };
+  double argVal = 0;
+  while (Serial.available()) { str = Serial.readStringUntil('\n'); }
+
+  if (str.length() > 0) {
+    
+    //Parse string function wasn't working on GIGA
+    char s[100] = { 0 };
+    int numArgs = 0;
+    int size = sizeof(argBuf);
+    strcpy(s, str.c_str());
+    argBuf[0] = strtok(s, " ");
+    //Serial.print(argBuf[0]);
+    for (numArgs = 1; NULL != (argBuf[numArgs] = strtok(NULL, " ")); numArgs++) {
+      //Serial.println(argBuf[numArgs] );
+      if (size == numArgs) { break; }
+    }
+    
+    for (int n = 0; n < numArgs; n++) {
+      String argStr = argBuf[n];
+      int digital = false;
+      for(int i = 0; i < DOsize; i++){
+        if(argStr.equalsIgnoreCase(DOdata[i].key)){
+          String argStrVal = argBuf[++n];
+          if(argStrVal.equalsIgnoreCase("false")){
+            *DOdata[i].value = false;
+          }
+          else if(argStrVal.equalsIgnoreCase("true")){
+            *DOdata[i].value = true;
+          }
+          else{ *DOdata[i].value = !*DOdata[i].value; }
+          digital = true;
+          break;
+        }
+      }
+      if(digital){;}
+
+       else if(argStr.equalsIgnoreCase("sw1")) {
+        String argStrVal = argBuf[++n];
+        argVal = argStrVal.toInt();
+        if(argVal > 0){switchingPsi1 = argVal;}
+      }
+
+      else if(argStr.equalsIgnoreCase("sw2")) {
+        String argStrVal = argBuf[++n];
+        argVal = argStrVal.toInt();
+        if(argVal > 0){switchingPsi1 = argVal;}
+      }
+
+      else if(argStr.equalsIgnoreCase("delay")) {
+        String argStrVal = argBuf[++n];
+        argVal = argStrVal.toInt();
+        if(argVal > 0){delayTime = argVal;}
+      }
+   
+      else if(argStr.equalsIgnoreCase("help") || argStr.equalsIgnoreCase("h")){
+        printHelp();
+      }
+
+      else if(argStr.equalsIgnoreCase("lsr")){
+        lsrReset = 1;
+      }
+      
+      else if(argStr.equalsIgnoreCase("c50setup")){
+        c50setup = !c50setup;
+      }
+      else if(argStr.equalsIgnoreCase("raw")){
+        rawPrint = !rawPrint;
+      }
+
+      else if(argStr.equalsIgnoreCase("packet")){
+        printMode = (printMode == PACKET)?NONE:PACKET;
+      }
+
+      else if(argStr.equalsIgnoreCase("alldata")){
+        printMode = (printMode == ALL)?NONE:ALL;
+      }
+
+      else if(argStr.equalsIgnoreCase("di")){
+        printMode = (printMode == DIGITAL_IN)?NONE:DIGITAL_IN;
+      }
+
+      else if(argStr.equalsIgnoreCase("do")){
+        printMode = (printMode == DIGITAL_OUT)?NONE:DIGITAL_OUT;
+      }
+      else if(argStr.equalsIgnoreCase("tt")){
+        printMode = (printMode == TT)?NONE:TT;
+      }
+
+      else if(argStr.equalsIgnoreCase("pt")){
+        printMode = (printMode == PT)?NONE:PT;
+      }
+
+      else if(argStr.equalsIgnoreCase("debug")){
+        printMode = (printMode == debug)?NONE:debug;
+      }
+
+      else if(argStr.equalsIgnoreCase("plot")){
+        plot = !plot;
+      }
+      else if(argStr.equalsIgnoreCase("err")){
+        if(errMsg[0] == ""){Serial.println("No PAUSE Errors");}
+        else{
+          for(int h = 0; h < 30; h++){
+            if(errMsg[h] != ""){Serial.print(errMsg[h]);}
+            else{break;}
+          }
+        }
+      }
+        
+      else if(argStr.equalsIgnoreCase("pretty")){
+        prettyPrint = !prettyPrint;
+      }
+      else if(argStr.equalsIgnoreCase("manual")){
+        manualMode = !manualMode;
+        if(manualMode){
+          PREV_STATE = STATE;
+          STATE = MANUAL_CONTROL;
+        }
+        else{
+          STATE = PREV_STATE;
+          PREV_STATE = IDLE_OFF;
+        }
+      }
+
+      // else if(argStr.equalsIgnoreCase("keys")){
+      //   keyPrint = !keyPrint;
+      // }
+
+      else if(argStr.equalsIgnoreCase("stop") || argStr.equalsIgnoreCase("quiet")){
+        printMode = NONE;
+      }
+
+      else{
+        Serial.println("Invalid Entry");
+      }
+    }
+  }
+}
+
+void printHelp(){
+  Serial.println("################## HELP MENU ##################");
+  for(int i = 0; i < DOsize; i++){
+    String cc = DOdata[i].key;
+    for(int b = 1;b < (19 - cc.length());b++){
+      cc = cc + " ";
+    }
+    cc = cc + "-> Toggle " + DOdata[i].name;
+    Serial.println(cc);
+  }
+  Serial.println("off                -> Turn off ALL devices");
+  Serial.println("packet             -> Print data in standardized packet format");
+  Serial.println("di                 -> Print all gpio digital inputs");
+  Serial.println("do                 -> Print all gpio digital outputs");
+  Serial.println("pt                 -> Print all PT values");
+  Serial.println("tt                 -> Print all TT values");
+  Serial.println("printall           -> Print all data");
+  Serial.println("delay              -> Designate print delay time in ms (default: 1000)");
+  Serial.println("sw1                -> Change switching pressure 1");
+  Serial.println("sw2                -> Change switching pressure 2");
+  Serial.println("lsr                -> Virtually press lsr reset button for 500ms");
+  Serial.println("c50setup           -> paul's feature");
+  Serial.println("pretty             -> Toggle print mode labeled lists <--> csv list");
+  //Serial.println("keys               -> Toggle print mode labeled keys <--> csv list");
+  Serial.println("raw                -> Toggle to print only raw values");
+  Serial.println("plot               -> Toggle Plotting of all values using Arduino Serial Plotter");
+  Serial.println("stop/quiet         -> Silence all printouts");
+  Serial.println("manual             -> Control all output manually");
+  Serial.println("help/h             -> This help menu");
+  Serial.println("###############################################");
+
+}
+
+void dataPrint(unsigned long dly){
+  if(!dataPrintTimer){dataPrintTimer = millis();}
+  if(plot){
+    Serial.print("AI_HYD_psig_PT561_HydraulicInlet2:");
+    Serial.print(AI_HYD_psig_PT561_HydraulicInlet2);
+    Serial.print(" DO_HYD_XV554_DCV2_A:");
+    Serial.print(DO_HYD_XV554_DCV2_A*2000);
+    Serial.print(" DO_HYD_XV557_DCV2_B:");
+    Serial.print(DO_HYD_XV557_DCV2_B*2000);
+    Serial.print(" SwitchingPressure:");
+    Serial.print(switchingPsi1);
+    Serial.print(" AI_H2_psig_PT712_Stage1_DischargeTank:");
+    Serial.print(AI_H2_psig_PT712_Stage1_DischargeTank);
+    Serial.print(" AI_H2_psig_PT407_Stage3_Discharge:");
+    Serial.print(AI_H2_psig_PT407_Stage3_Discharge);
+    Serial.print(" AI_H2_psig_PT410_Stage3_DischargeTank :");
+    Serial.print(AI_H2_psig_PT410_Stage3_DischargeTank );
+    Serial.print(" AI_HYD_C_TT454_HydraulicTank:");
+    Serial.print(AI_HYD_C_TT454_HydraulicTank);
+    Serial.print(" AI_CLT_C_TT207_CoolantSupply2:");
+    Serial.print(AI_CLT_C_TT207_CoolantSupply2);
+    Serial.print(" AI_H2_C_TT715_Stage2_SuctionTank  :");
+    Serial.print(AI_H2_C_TT715_Stage2_SuctionTank  );
+    Serial.print(" AI_H2_C_TT520_Stage2_Discharge :");
+    Serial.print(AI_H2_C_TT520_Stage2_Discharge );
+    Serial.print(" AI_H2_C_TT521_Stage3_Suction :");
+    Serial.println(AI_H2_C_TT521_Stage3_Suction );
+    Serial.print(" AI_H2_C_TT522_Stage3_Discharge :");
+    Serial.println(AI_H2_C_TT522_Stage3_Discharge );
+  }
+  else if(millis() - dataPrintTimer > dly && dataPrintTimer){
+    if(!printMode){return;}
+    String msg = "{";
+    switch(printMode){
+      case PACKET:
+        for(int i = 0; i < TTsize;i++){
+          if(*TTdata[i].value != TTdata[i].prev){
+            if(msg != "{"){msg = msg + ",";}
+            msg = msg + "\"" + TTdata[i].key + "\":" + *TTdata[i].value;
+            TTdata[i].prev = *TTdata[i].value;
+          }
+        }
+
+        for(int i = 0; i < PTsize;i++){
+          if(*PTdata[i].value != PTdata[i].prev){
+            if(msg != "{"){msg = msg + ",";}
+            msg = msg + "\"" + PTdata[i].key + "\":" + *PTdata[i].value;
+            PTdata[i].prev = *PTdata[i].value;
+          }
+        }
+
+        for(int i = 0; i < DOsize;i++){
+          if(*DOdata[i].value != DOdata[i].prev){
+            if(msg != "{"){msg = msg + ",";}
+            msg = msg + "\"" + DOdata[i].key + "\":" + *DOdata[i].value;
+            DOdata[i].prev = *DOdata[i].value;
+          }
+        }
+
+        for(int i = 0; i < DIsize;i++){
+          if(*DIdata[i].value != DIdata[i].prev){
+            if(msg != "{"){msg = msg + ",";}
+            msg = msg + "\"" + DIdata[i].key + "\":" + *DIdata[i].value;
+            DIdata[i].prev = *DIdata[i].value;
+          }
+        }
+        for(int i = 0; i < varSize;i++){
+          if(*varData[i].value != varData[i].prev){
+            if(msg != "{"){msg = msg + ",";}
+            msg = msg + "\"" + varData[i].key + "\":" + *varData[i].value;
+            varData[i].prev = *varData[i].value;
+          }
+        }
+        if(msg != "{"){
+          msg = msg + "}";
+          Serial.println(msg);
+        }
+      break;
+
+      case TT:
+        for(int i = 0; i < TTsize; i++){
+          if(prettyPrint){
+            Serial.print("\"");
+            Serial.print(TTdata[i].name);
+            Serial.print("\": ");
+          }
+          Serial.print(rawPrint?TTdata[i].raw:*TTdata[i].value);
+          if(prettyPrint){ Serial.println(); }
+          else{ Serial.print(", "); }
+        }
+        Serial.println();
+      break;
+      
+      case PT:
+        for(int i = 0; i < PTsize; i++){
+          if(prettyPrint){
+            Serial.print("\"");
+            Serial.print(PTdata[i].name);
+            Serial.print("\": ");
+          }
+          Serial.print(rawPrint?PTdata[i].raw:*PTdata[i].value);
+          if(prettyPrint){ Serial.println(); }
+          else{ Serial.print(", "); }
+        }
+        Serial.println();
+      break;
+
+      case DIGITAL_IN:
+        for(int i = 0; i < DIsize; i++){
+          if(prettyPrint){
+            Serial.print("\"");
+            Serial.print(DIdata[i].name);
+            Serial.print("\": ");
+          }
+          Serial.print(*DIdata[i].value);
+          if(prettyPrint){ Serial.println(); }
+          else{ Serial.print(", "); }
+        }
+        Serial.println();
+      break;
+
+      case DIGITAL_OUT:
+        for(int i = 0; i < DOsize; i++){
+          if(prettyPrint){
+            Serial.print("\"");
+            Serial.print(DOdata[i].name);
+            Serial.print("\": ");
+          }
+          Serial.print(*DOdata[i].value);
+          if(prettyPrint){ Serial.println(); }
+          else{ Serial.print(", "); }
+        }
+        Serial.println();
+      break;
+    
+    case debug:
+      Serial.print("LoopTime: ");
+      Serial.println(loopTime);
+      
+      Serial.print("STATE: ");
+      Serial.println(STATE);
+
+      Serial.print("Intensifier STATE: ");
+      Serial.println(INTENSE2);
+
+      Serial.print("timer4: ");
+      Serial.println(millis() - timer[4]);
+
+      Serial.print("DO_HYD_XV554_DCV2_A: ");
+      Serial.println(DO_HYD_XV554_DCV2_A);
+
+      Serial.print("DO_HYD_XV557_DCV2_B: ");
+      Serial.println(DO_HYD_XV557_DCV2_B);
+
+      Serial.print("hydraulic/switching: ");
+      Serial.print(AI_HYD_psig_PT561_HydraulicInlet2);
+      Serial.print(" / ");
+      Serial.println(switchingPsi2);
+
+      Serial.print("suction: ");
+      Serial.println(AI_H2_psig_PT712_Stage1_DischargeTank);
+      Serial.print("max discharge3: ");
+      Serial.println(PTdata[1].max);
+    break;
+
+    default:
+    break;
+    }
+    dataPrintTimer = millis();
+  }
+}
+ 
